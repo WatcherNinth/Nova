f1bf6e0b07dde66e4adfe9e38eb87456197fc506
WatcherNinth <38343739+WatcherNinth@users.noreply.github.com>
2025-03-31 06:30:10 +0800

    加入推理按钮的论点论据区分

--- Assets/NinthsLab/Scripts/UI/DeductionList/DeductionListUIDeductionButtonScript.cs
+++ Assets/NinthsLab/Scripts/UI/DeductionList/DeductionListUIDeductionButtonScript.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class DeductionListUIDeductionButtonScript : MonoBehaviour
 {
+    enum e_DeductionType
+    {
+        Topic,
+        Proof,
+        Unassigned,
+    }
     public TMP_Text deductionNameText;
     public Interrorgation_Deduction DeductionData;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,17 +22,40 @@ public class DeductionListUIDeductionButtonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void InitButton(Interrorgation_Deduction deduction)
     {
         DeductionData = deduction;
-        deductionNameText.text = deduction.DeductionText;
+        var text = deduction.DeductionText;
+        //临时代码，之后要改
+        if (getDeductionType(deduction) == e_DeductionType.Topic)
+        {
+            text = "论点：" + text;
+        }
+        else
+        {
+            text = "论据：" + text;
+        }
+        deductionNameText.text = text;
     }
 
     public void OnButtonClick()
     {
         DeductionUIManager.Instance.SelectDeduction(DeductionData);
+    }
+
+    e_DeductionType getDeductionType(Interrorgation_Deduction deduction)
+    {
+        switch (deduction)
+        {
+            case Interrorgation_Topic:
+                return e_DeductionType.Topic;
+            case Interrorgation_Proof:
+                return e_DeductionType.Proof;
+            default:
+                return e_DeductionType.Unassigned; // 或者抛出异常
+        }
     }
 }
