f5e347f381008c5b97dcf9fd75d170562bff27bd
WatcherNinth <38343739+WatcherNinth@users.noreply.github.com>
2025-12-12 16:58:47 +0800

    开始写playerMindmapmanager

--- Assets/NinthsLab/Scripts/InterrorgationLevelScript/Logic/PlayerMindMapManager.cs
+++ Assets/NinthsLab/Scripts/InterrorgationLevelScript/Logic/PlayerMindMapManager.cs
@@ -1,9 +1,95 @@
-using Interrorgation.MidLayer;
+using System.Collections.Generic;
+using AIEngine.Network;
+using LogicEngine.LevelGraph;
+
 
 namespace LogicEngine.LevelLogic
 {
     public class PlayerMindMapManager
     {
+        readonly LevelGraphData levelGraph;
 
+        public List<RuntimeNodeData> RunTimeNodeDataList = new List<RuntimeNodeData>();
+
+        public List<RuntimeEntityItemData> RunTimeEntityItemDataList = new List<RuntimeEntityItemData>();
+
+        public List<RuntimeTemplateData> RunTimeTemplateDataList = new List<RuntimeTemplateData>();
+
+
+        public PlayerMindMapManager(ref LevelGraphData inputLevelGraph)
+        {
+            levelGraph = inputLevelGraph;
+        }
+
+        void InitializeRuntimeData()
+        {
+            //[AITask]根据LevelGraphData生成RuntimeNodeDataList, RunTimeEntityItemDataList, RunTimeTemplateDataList，默认状态为Hidden
+        }
+
+        //[AITask]参考下面的GetRuntimeNodeDataByStatus结构，完成三个数据结构按status搜索的方法
+        public List<RuntimeNodeData> GetRuntimeNodeDataByStatus(RunTimeNodeStatus runTimeNodeStatus)
+        {
+            return null;
+        }
+        
+
+        public void ProcessAIResponse(AIResponseData responseData)
+        {
+            // 处理AI响应数据
+        }
+
+
+    }
+
+    public enum RunTimeNodeStatus
+    {
+        Hidden,
+        Discovered,
+        Submitted,
+    }
+    public class RuntimeNodeData
+    {
+        public readonly NodeData r_NodeData;
+        public RunTimeNodeStatus Status;
+        public RuntimeNodeData(NodeData nodeData, RunTimeNodeStatus status)
+        {
+            this.r_NodeData = nodeData;
+            this.Status = status;
+        }
+    }
+
+    public enum RunTimeEntityItemStatus
+    {
+        Hidden,
+        Discovered,
+    }
+
+    public class RuntimeEntityItemData
+    {
+        public readonly EntityItem r_EntityItemData;
+        public RunTimeEntityItemStatus Status;
+        public RuntimeEntityItemData(EntityItem Data, RunTimeEntityItemStatus status)
+        {
+            this.r_EntityItemData = Data;
+            this.Status = status;
+        }
+    }
+
+    public enum RunTimeTemplateDataStatus
+    {
+        Hidden,
+        Discovered,
+        Used,
+    }
+
+    public class RuntimeTemplateData
+    {
+        public readonly TemplateData r_TemplateData;
+        public RunTimeTemplateDataStatus Status;
+        public RuntimeTemplateData(TemplateData Data, RunTimeTemplateDataStatus status)
+        {
+            this.r_TemplateData = Data;
+            this.Status = status;
+        }
     }
 }
--- Assets/NinthsLab/Scripts/InterrorgationLevelScript/MidLayer/UIEventDispatcher.cs
+++ Assets/NinthsLab/Scripts/InterrorgationLevelScript/MidLayer/UIEventDispatcher.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using LogicEngine;
+using LogicEngine.LevelGraph;
 
 namespace Interrorgation.MidLayer
 {
@@ -8,6 +11,26 @@ namespace Interrorgation.MidLayer
         public static void DispatchPlayerSubmitInput(string input)
         {
             OnPlayerSubmitInput?.Invoke(input);
+        }
+
+        public static event Action<List<NodeData>> OnDiscoveredNewNodes;
+
+        public static void DispatchDiscoveredNewNodes(List<NodeData> nodes)
+        {
+            OnDiscoveredNewNodes?.Invoke(nodes);
+        }
+
+        public static event Action<List<EntityItem>> OnDiscoveredNewEntity;
+
+        public static void DispatchDiscoveredNewNodes(List<EntityItem> entityItems)
+        {
+            OnDiscoveredNewEntity?.Invoke(entityItems);
+        }
+
+        public static event Action<List<TemplateData>> OnDiscoveredNewTemplates;
+        public static void DispatchDiscoveredNewTemplates(List<TemplateData> templates)
+        {
+            OnDiscoveredNewTemplates?.Invoke(templates);
         }
     }
 }
